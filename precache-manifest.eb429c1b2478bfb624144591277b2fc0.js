50060f656b70a23d28fe789ccb6a954821bd3401
Nik <lavrish-nikita@yandex.ru>
2020-06-12 12:47:23 +0300

    Updates

---
+++ precache-manifest.eb429c1b2478bfb624144591277b2fc0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27441cf9444d7302aab808c79ac97cd8",
+    "url": "/gameOnReact/index.html"
+  },
+  {
+    "revision": "8cca807191881dd517aa",
+    "url": "/gameOnReact/static/css/main.4dd54b82.chunk.css"
+  },
+  {
+    "revision": "0af4002f6b6ec5ec6aaa",
+    "url": "/gameOnReact/static/js/2.de0d9d9f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/gameOnReact/static/js/2.de0d9d9f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8cca807191881dd517aa",
+    "url": "/gameOnReact/static/js/main.8aed3331.chunk.js"
+  },
+  {
+    "revision": "76b7e869a051e918f653",
+    "url": "/gameOnReact/static/js/runtime-main.12e01897.js"
+  }
+]);
